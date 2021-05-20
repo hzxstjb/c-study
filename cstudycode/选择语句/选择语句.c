245d2f33559e7aa3c45bebf4f63ff79aabe79c91
hzxstjb <tangjb_ok@163.com>
2021-05-20 21:56:40 +0800

    20210520
    c study code github

---
+++ cstudycode/选择语句/选择语句.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main(void) {
+
+	int age = 20;
+
+	// if
+/*
+	if (18 > age) {
+
+		printf("未成年\n");
+	}
+*/
+
+
+	// if ... else ...
+/*
+	if (18 > age) {
+		printf("未成年\n");
+	}
+	else {
+		printf("已成年\n");
+	}*/
+
+	return 0;
+}
