e76425721a8e60cf93f532f0f537c5bf5dd352e7
hzxstjb <tangjb_ok@163.com>
2021-05-21 17:28:19 +0800

    20210521

--- cstudycode/打印语句/printf语句.c
+++ cstudycode/打印语句/printf语句.c
@@ -5,5 +5,12 @@
 int main(void) {
 	// 输出占位:%d 整数, %c 字符, %s 字符串, %f 单精度浮点数, %lf 双精度浮点数
 	printf("%d,  %c,  %s,  %f,  %lf\n", 10, 'a', "hello", 3.14f, 6.01);
+	
+	// %u 无符号整型
+
+	printf("%u\n", 1);
+
+	// %ld 长整型
+	printf("%ld\n", 1L);
 	return 0;
 }
