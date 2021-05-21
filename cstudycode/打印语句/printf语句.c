c1c86fd6e79cb680f8bf82b48358f3ce91e0de81
hzxstjb <tangjb_ok@163.com>
2021-05-21 17:11:42 +0800

    20210521

--- cstudycode/打印语句/printf语句.c
+++ cstudycode/打印语句/printf语句.c
@@ -1,1 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main(void) {
+	// 输出占位:%d 整数, %c 字符, %s 字符串, %f 单精度浮点数, %lf 双精度浮点数
+	printf("%d,  %c,  %s,  %f,  %lf\n", 10, 'a', "hello", 3.14f, 6.01);
+	return 0;
+}
