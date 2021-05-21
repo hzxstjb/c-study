696c826fb38e7a858b78b53ec71f2be20a49f1e3
hzxstjb <tangjb_ok@163.com>
2021-05-22 07:11:51 +0800

    20210522

--- cstudycode/循环语句/for循环的变型.c
+++ cstudycode/循环语句/for循环的变型.c
@@ -4,14 +4,32 @@
 
 int main(void) {
 	// 1
-	/*int x, y;
-	for (x = 0, y = 0; x <= 2 && y < 5; x++, y++) {
-		printf("x=%d, y=%d\n", x, y);
-	}*/
+
+	//int x, y;
+	//for (x = 0, y = 0; x <= 2 && y < 5; x++, y++) {	// 多个变量用逗号隔开
+	//	printf("x=%d, y=%d\n", x, y);
+	//}
+
 	// 2 for实现无限循环
-	for (;;) {
-		printf("hello\n");
+
+	//for (;;) {	// 等同于while(1){ ... }
+	//	printf("hello\n");
+	//}
+
+	// 3 
+
+	//int i = 0;
+	//for (i = 1;; i++) {	// for(exp1;exp2;exp3)  如果exp2为空,即表示条件为恒真,进入无限循环
+	//	printf("%d", i);
+	//}
+
+	//	4
+
+	int i = 0;
+	for (i = 1; 0; i++) {	// exp2为0,表示条件恒为假,永远不能进入循环
+		printf("haha");
 	}
-	// 3
+
+
 	return 0;
 }
---
+++ cstudycode/打印语句/scanf语句.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+
+	int age = 0;
+	char name[10] = { 0 };
+	printf("请输入你的姓名\n");
+	scanf("%s", name);
+	printf("请输入你的年龄\n");
+	scanf("%d", &age);
+
+	printf("你的名字叫:%s, 你的年龄:%d\n", name, age);
+	getchar();
+	return 0;
+}
