976559df6e10a96c8c969f85c156b95a551c734d
hzxstjb <tangjb_ok@163.com>
2021-05-26 16:06:30 +0800

    20210526

---
+++ cstudycode/函数类型/函数2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+	// ×Ö·û´®²Ù×÷º¯Êý
+	//strcpy() ×Ö·û´®¸´ÖÆ
+	//strcmp() ×Ö·û´®±È½Ï
+
+	return 0;
+}
