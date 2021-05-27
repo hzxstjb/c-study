85fd55e60415997fea1498402bbbbacb5e3e9109
hzxstjb <tangjb_ok@163.com>
2021-05-27 13:06:03 +0800

    20210527

---
+++ cstudycode/函数类型/函数练习1.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <math.h>
+//int isPrimer(int n) {
+//	int i = 0;
+//	for (i = 2; i < n; i++) {
+//		if (n % i == 0) {
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+
+// 优化
+int isPrimer(int n) {
+	int i = 0;
+	for (i = 2; i <= sqrt(n); i++) {	//sqrt()开平方函数,徐导入math.h库
+		if (n % i == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+int main(void) {
+
+	// 用函数的方式实现素数的判断
+
+	int num = 0;
+	for (num = 100; num <= 200; num++) {
+		if (isPrimer(num) == 1) {
+			printf("%d\n", num);
+		}
+	}
+	return 0;
+}
